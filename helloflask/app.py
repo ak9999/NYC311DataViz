a88510e26029a0e7be43e28a47bcf75324124095
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-27 11:53:05 -0400

    Simple Bootstrap Flask page.

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from flask import send_from_directory
 from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')  # Decorator tells Flask what URL should trigger our function
-def introduction():
+def index():
 	"""Returns a static page."""
-	return render_template('index2.html')
+	return render_template('index.html')
 '''
 To actually run this, we need to export the environment variable
 "FLASK_APP", ex: export FLASK_APP=hello.py
