a1d9e4a2fba37cd565da0771c6c39190615914ed
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-27 13:04:58 -0400

    about url

--- app/app.py
+++ app/app.py
@@ -13,6 +13,14 @@ def index():
 		function. In this case, '/' is our index.html
 	"""
 	return render_template('index.html')  # Renders index.html
+
+
+@app.route('/about')
+def about():
+    """Returns the about page, which for now is just index.html"""
+    return render_template('about.html')
+
+
 '''
 To actually run this, we need to export the environment variable
 "FLASK_APP", ex: export FLASK_APP=app.py
