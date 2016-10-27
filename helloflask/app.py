8176ea7fbd82606f56318188d2a44aadf66e78d5
Abdullah Khan <khanabdullahj@gmail.com>
2016-10-27 11:51:24 -0400

    Renamed file to avoid conflicts

--- helloflask/app.py
+++ helloflask/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import send_from_directory
+from flask import render_template
 
-app = Flask(__name__, static_url_path='./introduction/')
+app = Flask(__name__)
 
 
 @app.route('/')  # Decorator tells Flask what URL should trigger our function
 def introduction():
 	"""Returns a static page."""
-	return send_from_directory('./introduction/', 'index.html')
+	return render_template('index2.html')
 '''
 To actually run this, we need to export the environment variable
 "FLASK_APP", ex: export FLASK_APP=hello.py
