4d7accd96a2cf3b013d321784210688db0ffcc11
Abdullah Khan <khanabdullahj@gmail.com>
2016-11-07 14:00:46 -0500

    Make query functions cleaner.

--- app/app.py
+++ app/app.py
@@ -24,31 +24,54 @@ def about():
 	"""Returns the about page, which for now is just index.html"""
 	return render_template('about.html')
 
+
 @app.route('/query', methods=['GET'])
 @app.route('/query/', methods=['GET'])
 @app.route('/query/<request_type>', methods=['GET'])
 def query(column='Complaint Type', request_type='Special Enforcement'):
 	'''Returns Latitude and Longitude of issues.'''
-	def show_it():
-		# Connect to database
-		try:
-			client = MongoClient('mongodb://localhost:27017/')
-			print('Connection successful.')
-		except:
-			print('Could not connect to MongoDB')
-			return
+	# Connect to database
+	try:
+		client = MongoClient('mongodb://localhost:27017/')
+		print('Connection successful.')
+	except:
+		print('Could not connect to MongoDB')
+		return
 
 
-		db = client.requests  # Database
-		collection = db.sr    # Collection within database
-		projection = {'_id': False, 'Latitude': True, 'Longitude': True}  # Properties we want.
-		coordinates = collection.find({column: request_type}, projection) # MongoDB Cursor object, iterable.
-		print('Successfully obtained {} coordinates.'.format(coordinates.count()))
-		return json_util.dumps({'latlong': coordinates})  # Convert query results to json and return
+	db = client.requests  # Database
+	collection = db.sr    # Collection within database
+	projection = {'_id': False, 'Latitude': True, 'Longitude': True}  # Properties we want.
+	coordinates = collection.find({column: request_type}, projection) # MongoDB Cursor object, iterable.
+	print('Successfully obtained {} coordinates.'.format(coordinates.count()))
 
-	return Response(show_it(), mimetype='application/json')  # Return the result of show_it()
+	def jsonify(query_result):
+		return json_util.dumps({'latlong': query_result})  # Convert query results to json and return
+
+	return Response(jsonify(coordinates), mimetype='application/json')  # Return the result of show_it()
 
 
+@app.route('/nypd', methods=['GET'])
+def GetNYPDResponses():
+	'''Returns JSON object of coordinates for each instance responded to by the NYPD.'''
+	# Connect to database
+	try:
+		client = MongoClient('mongodb://localhost:27017/')
+		print('Connection successful.')
+	except:
+		print('Could not connect to MongoDB')
+		return
+
+	db = client.requests  # Database
+	collection = db.sr    # Collection within database
+	projection = {'_id': False, 'Latitude': True, 'Longitude': True}  # Properties we want.
+	coordinates = collection.find({'Agency': 'NYPD'}, projection) # MongoDB Cursor object, iterable.
+	print('Successfully obtained {} coordinates.'.format(coordinates.count()))
+
+	def jsonify(query_result):
+		return json_util.dumps({'latlong': query_result})  # Convert query results to json and return
+
+	return Response(jsonify(coordinates), mimetype='application/json')
 # Run within virtual environment with python3 app/app.py
 
 if __name__ == '__main__':
