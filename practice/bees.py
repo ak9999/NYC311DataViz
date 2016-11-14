76b0ff761f2c266490969d8bed48ff9c9154e9c4
Abdullah Khan <khanabdullahj@gmail.com>
2016-11-14 14:41:33 -0500

    Add HTML popups.

--- practice/bees.py
+++ practice/bees.py
@@ -15,7 +15,7 @@ except:
 
 db = client.requests  # Database
 collection = db.sr    # Collection within database
-projection = {'_id': False, 'Latitude': True, 'Longitude': True, 'Agency': True}  # Properties we want.
+projection = {'_id': False, 'Latitude': True, 'Longitude': True, 'Descriptor': True, 'Agency': True}  # Properties we want.
 
 coordinates = collection.find({'Complaint Type': 'Harboring Bees/Wasps'}, projection)
 print('Successfully obtained {} coordinates.'.format(coordinates.count()))
@@ -23,13 +23,15 @@ print('Successfully obtained {} coordinates.'.format(coordinates.count()))
 hc_coords = (40.768890, -73.964789)
 
 # Create empty map zoomed in on Hunter College
-map_1 = folium.Map(location=hc_coords, zoom_start=12)
+bees_map = folium.Map(location=hc_coords, zoom_start=12)
 
 print('Inserting points.')
 for each in coordinates:
     if each['Latitude'] and each['Longitude']:
-        folium.CircleMarker([each['Latitude'], each['Longitude']],
-                            radius=1).add_to(map_1)
+        description = '<p>Responding Agency: {}<br>Description: {}</p>'.format(each['Agency'], each['Descriptor'])
+        iframe = folium.element.IFrame(html=description, width=300, height=85)
+        popup = folium.Popup(iframe, max_width=2650)
+        folium.Marker([each['Latitude'], each['Longitude']], popup=popup).add_to(bees_map)
 
-map_1.save('map1.html')
+bees_map.save('bees.html')
 print('Saved map.')
