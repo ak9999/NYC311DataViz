181462e76cb93d53aaa38b9483b5d285a2688746
Abdullah Khan <khanabdullahj@gmail.com>
2016-11-14 14:14:15 -0500

    ???

--- app/static/js/simplequery.js
+++ app/static/js/simplequery.js
@@ -1,14 +1,10 @@
-var myreq = new Request('http://localhost:5000/bees');
-
-var bees = fetch(myreq)
-	.then(function(response) {
-		if(response.status == 200) return response.json();
-		else throw new Error('Response is NOT OK');
-	})
-	.catch(function(error) {
-		console.error(error);
-	});
-
-for (var i = 0; i < bees.length; i++) {
-	console.log(bees[i]);
+function httpGetAsync(qurl, callback)
+{
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", qurl, true); // true for async
+	xmlHttp.send(null);
 }
